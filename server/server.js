69cbf593bc0b5707b9f42ac1e4766a8e6830d3ad
Lavanya Sandula <47265625+Lavany549@users.noreply.github.com>
2024-02-16 00:39:38 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const crypto = require("crypto");
+const path = require('path');
+require("dotenv").config();
+
+const app = express();
+
+//middlewares
+const corsOptions ={
+  origin: "http://localhost:3000"
+}
+
+const dbConfig = require('./DB');
+
+const usersRoute = require('./routes/usersRoute');
+const dataRoute = require('./routes/dataRoute');
+
+
+
+// app.use(express.static(path.join(__dirname+"/public")))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+
+app.use('/api/users', usersRoute);
+app.use('/api/data', dataRoute);
+
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => console.log('Node server started using nodemon',port));
